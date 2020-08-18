14c0eed9f444a2db750e099ca9996750932a3a8c
Christy Patrick <christy06p@msn.com>
2020-08-17 20:43:33 -0500

    setup code to read csv file

---
+++ PyBank/main.py
@@ -0,0 +1,36 @@
+# import OS module
+import os
+
+# import CSV module
+import csv
+
+# set file path
+csv_path = os.path.join('Resources', 'budget_data.csv')
+
+# Read using CSV module
+
+with open(csv_path, 'r') as csv_file:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csv_reader = csv.reader(csv_file, delimiter=',')
+
+    print(csv_reader)
+
+    # Read the header row first (skip this step if there is no header)
+    csv_header = next(csv_reader)
+    print(f"CSV Header: {csv_header}")
+
+    # Read each row of data after the header
+    for row in csv_reader:
+        print(row)
+
+
+# months in data
+
+# net total amount of profit/losses
+
+# average of changes in profits/losses
+
+# month with the greatest increase (include amount)
+
+#  month with the greatest decrease (include amount)
