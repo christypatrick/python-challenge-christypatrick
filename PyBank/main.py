b56cfab1bf9207fabf4f16bd1abe4a3069566b38
Christy Patrick <christy06p@msn.com>
2020-08-19 12:31:36 -0500

    count months and total profit

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,6 +7,10 @@ import csv
 # set file path
 csv_path = os.path.join('Resources', 'budget_data.csv')
 
+# Variables
+num_months = 0
+total_profit = 0
+
 # Read using CSV module
 
 with open(csv_path, 'r') as csv_file:
@@ -14,32 +18,39 @@ with open(csv_path, 'r') as csv_file:
     # CSV reader specifies delimiter and variable that holds contents
     csv_reader = csv.reader(csv_file, delimiter=',')
 
-    print(csv_reader)
+    #print(csv_reader)
 
     # Read the header row first (skip this step if there is no header)
     csv_header = next(csv_reader)
-    # print(f"CSV Header: {csv_header}")
+    #print(f"CSV Header: {csv_header}")
 
     # Read each row of data after the header
+    #for row in csv_reader:
+        #print(row)
+
+    #print(mydict)
+
     for row in csv_reader:
-        # print(row)
-
-
-# Analysis Header
-        print("")
-        print("Financial Analysis")
-        print("-----------------------------------------")
-
-# months in data - count months in the list and capture the corresponding candy at each value of i
-    #for i in range(len(date)):
-        num_months = len(list(csv_reader))
-        print(f"Total Months: {num_months}")
-
-# net total amount of profit/losses
-
-
+        
+    # months in data - count months in the list 
+        num_months = num_months + 1
+      
+    # net total amount of profit/losses
+        total_profit = total_profit +int(row[1])
+    
+    
 # average of changes in profits/losses
+    
 
 # month with the greatest increase (include amount)
 
 #  month with the greatest decrease (include amount)
+
+    # Analysis Header
+    print("")
+    print("Financial Analysis")
+    print("-"*30)   
+
+    #print analysis
+    print(f"Total Months: {num_months}")  
+    print(f"Total: $ {total_profit}")
