56abef0085b60b23c016bf694fd4ccbb0811f020
Christy Patrick <christy06p@msn.com>
2020-08-19 22:37:04 -0500

    cleaned up formatting

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,17 +7,24 @@ import csv
 # set file path
 csv_path = os.path.join('Resources', 'budget_data.csv')
 
-# Variables
+### Variables
+# Used to calculate number of months
 num_months = 0
+# Used to calculate the total profit or loss
 total_profit = 0
+# Used when calculating the monthly change in profit or loss
 previous_revenue = 0
 monthly_change = 0
+# Calculates average change
 average_change = 0
-# greatest_increase = 0
-# greatest_inc_month = 0
-# greatest_decrease = 0
-# greatest_dec_month = 0
+#Used when identifying the month with the highest change month to month
+greatest_increase = 0
+greatest_inc_month = 0
+#Used when identifying the month with the lowest change month to month
+greatest_decrease = 0
+greatest_dec_month = 0
 
+#List to hold month to month changes in profit
 profit_changes = []
 
 # Read using CSV module
@@ -35,15 +42,12 @@ with open(csv_path, 'r') as csv_file:
 
     # Read each row of data after the header
     #for row in csv_reader:
-        #print(row)
-
-    #print(mydict)
-
+        #print(row) 
 
     for row in csv_reader:
-        
-    # months in data - count months in the list 
-        num_months = num_months + 1
+
+        #months in data - count months in the list 
+        num_months = num_months + 1    
       
     # net total amount of profit/losses
         total_profit = total_profit + int(row[1])
@@ -54,27 +58,26 @@ with open(csv_path, 'r') as csv_file:
         monthly_change = int(row[1]) - previous_revenue
 
         #reset the previous month's revenue to the row just above
-        #previous_revenue = int(row[1])
+        previous_revenue = int(row[1])
 
         #store monthly changes in a list to be summed up to 196785
-        #profit_changes.append()
-    print(monthly_change)
+        profit_changes.append(monthly_change)
+        print(profit_changes)
 
-    ##calculate the sum(profit_changes)/number of profit_changes
-    #average_change = str(sum(profit_changes)/len(profit_changes))
+    # calculate the sum(profit_changes)/number of profit_changes
 
+        average_change = int(sum(profit_changes)/len(profit_changes))
+        print(f"{average_change}")
 
-##month with the greatest increase (include amount)
-# If monthly_change > greatest_increase then:
-#   greatest_increase = monthly_change
-#   greatest_inc_month = (row[0])
+        # month with the greatest increase (include amount)
+        if (monthly_change > greatest_increase):
+            greatest_increase = monthly_change
+            greatest_inc_month = (row[0])
 
-
-##month with the greatest decrease (include amount)
-# If monthly_change < greatest_decrease then:
-#   greatest_decrease = monthly_change
-#   greatest_dec_month = (row[0])
-
+        #month with the greatest decrease (include amount)
+        if (monthly_change < greatest_decrease):
+            greatest_decrease = monthly_change
+            greatest_dec_month = (row[0])
 
     # Analysis Header
     print("")
@@ -84,6 +87,6 @@ with open(csv_path, 'r') as csv_file:
     #print analysis
     print(f"Total Months: {num_months}")  
     print(f"Total: $ {total_profit}")
-    #print(f"Average Change: $ {average_change}")
-    #print(f"Greatest Increase in Profits: {greatest_inc_month}($ {greatest increase})")
-    #print(f"Greatest Decrease in Profits: {greatest_dec_month}($ {greatest decrease})")
+    print(f"Average Change: $ {average_change}")
+    print(f"Greatest Increase in Profits: {greatest_inc_month}($ {greatest_increase})")
+    print(f"Greatest Decrease in Profits: {greatest_dec_month}($ {greatest_decrease})")
