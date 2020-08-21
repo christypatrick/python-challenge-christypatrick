0d735b322b348bd55e48c3ae3004c5f4d8752b24
Christy Patrick <christy06p@msn.com>
2020-08-21 17:29:28 -0500

    Created lists for the candidate votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,16 +9,21 @@ csv_path = os.path.join('Resources', 'election_data.csv')
 
 ### Variables
 num_votes = 0
-candidate_votes = 0  
+candidate_votes = 0
+
 
 candidate_list = []
+khan_votes = []
+correy_votes = []
+li_votes = []
+otooley_votes = []
 
 # Read using CSV module
 
 with open(csv_path, 'r') as csv_file:
 
     # CSV reader specifies delimiter and variable that holds contents
-    csv_reader = csv.DictReader(csv_file, delimiter=',')
+    csv_reader = csv.reader(csv_file, delimiter=',')
 
     #print(csv_reader)
 
@@ -36,14 +41,26 @@ with open(csv_path, 'r') as csv_file:
         num_votes = num_votes + 1  
 
     # Compile a list of candidates that received votes
-        if (row[2] not in candidate_list):
+        if (row[2]) not in candidate_list:
             candidate_list.append(row[2])
         else:
             pass        
 
-    print(candidate_list)
+    #print(candidate_list)
      #total number of votes each candidate won
-        #candidate_votes = len(list(row[0]))
+        if (row[2]) == "Khan":
+            khan_votes.append(row[2])
+        elif (row[2]) == "Correy":
+            correy_votes.append(row[2])
+        elif (row[2]) == "Li":
+            li_votes.append(row[2])
+        elif (row[2]) == "O'Tooley":
+            otooley_votes.append(row[2])
+        else:
+            pass 
+
+
+
     #unique List by candidate, if the name is the same then add it to the vote list. length of the list is the count - outside of the loop.
  
     # Percentage of votes each candidate won
@@ -59,7 +76,10 @@ with open(csv_path, 'r') as csv_file:
     print("-"*30)
     print(f"Total Votes: {num_votes}")    
     print("-"*30)
-    print (f"{row[2]}: {num_votes}")
+    print (f"Khan: {len(khan_votes)}")
+    print (f"Correy: {len(correy_votes)}")
+    print (f"Li: {len(li_votes)}")
+    print (f"O'Tooley: {len(otooley_votes)}")
     # #print candidate: % votes (num votes)
     # print("-"*30)
     # #print(f"Winner: {}")
